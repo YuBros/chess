4a569c7642b6292dd51944391f5a5c14c7c4f17c
Your Name <you@example.com>
2024-02-18 11:56:44 +0100

    second day

--- piezas.py
+++ piezas.py
@@ -1,14 +1,16 @@
 from enum import StrEnum
-from tblero import TableroAjedrez
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from tblero import TableroAjedrez
 class Pieza:
     def __init__(self,color):
         self.color=color
     def can_move(self,
                 from_position: tuple[int,int],
-                new_board:tuple[int,int],
-                tblero:TableroAjedrez
+                new_position:tuple[int,int],
+                tblero:'TableroAjedrez'
                 )-> bool:
-        row,column=new_board
+        row,column=new_position
         if tblero.tablero [column][row].color ==self.color:
             return False
         return True
@@ -20,10 +22,21 @@ class Color(StrEnum):
 class Peon(Pieza):
     def can_move(self,
                  from_position: tuple[int,int],
-                 new_board:tuple[int,int],
-                 tblero:TableroAjedrez)-> bool:
-        if not super().can_move(from_position, new_board, tblero):
+                 new_position:tuple[int,int],
+                 tblero:'TableroAjedrez')-> bool:
+        if not super().can_move(from_position, new_position, tblero):
             return False
+        from_row, from_column =from_position
+        to_row, to_column= new_position
+        row=abs(to_row-from_row)
+        column=abs(to_column-from_column)
+        if (
+            row>2 
+            or row<0 
+            or (row==1 and tblero[to_row][to_column] != " " and column == 0)
+        ):
+            return False
+        return True 
 class Alfil(Pieza):
     ...
 
--- tblero.py
+++ tblero.py
@@ -1,18 +1,18 @@
-from piezas import Peon,Alfil,Caballo,Torre,Reina,Rey,Color
+from piezas import Peon, Alfil, Caballo, Torre, Reina, Rey, Color
 
 class TableroAjedrez:
     def __init__(self):
         self.tablero=[
-            [Torre(Color.BLANCO,(0,0)),Caballo(Color.BLANCO,(0,1)),Alfil(Color.BLANCO,(0,2)),Reina(Color.BLANCO,(0,3)),Rey(Color.BLANCO,(0,4)),Alfil(Color.BLANCO,(0,5)),Caballo(Color.BLANCO,(0,6)),Torre(Color.BLANCO,(0,7))],
-            [Peon(Color.BLANCO),Peon(Color.BLANCO),Peon(Color.BLANCO),Peon(Color.BLANCO),Peon(Color.BLANCO),Peon(Color.BLANCO),Peon(Color.BLANCO),Peon(Color.BLANCO)],
-            [" "," "," "," "," "," "," "," "],
-            [" "," "," "," "," "," "," "," "],
-            [" "," "," "," "," "," "," "," "],
-            [" "," "," "," "," "," "," "," "],
-            [Peon(Color.NEGRO),Peon(Color.NEGRO),Peon(Color.NEGRO),Peon(Color.NEGRO),Peon(Color.NEGRO),Peon(Color.NEGRO),Peon(Color.NEGRO),Peon(Color.NEGRO)],
-            [Torre(Color.NEGRO),Caballo(Color.NEGRO),Alfil(Color.NEGRO),Reina(Color.NEGRO),Rey(Color.NEGRO),Alfil(Color.NEGRO),Caballo(Color.NEGRO),Torre(Color.NEGRO)],
-            ]
-    def mover_pieza(self,casilla_inicial,casilla_final):
+            [Torre(Color.BLANCO, (0, 0)), Caballo(Color.BLANCO, (0, 1)), Alfil(Color.BLANCO, (0, 2)), Reina(Color.BLANCO, (0, 3)), Rey(Color.BLANCO, (0, 4)), Alfil(Color.BLANCO, (0, 5)), Caballo(Color.BLANCO, (0, 6)), Torre(Color.BLANCO, (0, 7))], 
+            [Peon(Color.BLANCO), Peon(Color.BLANCO), Peon(Color.BLANCO), Peon(Color.BLANCO), Peon(Color.BLANCO), Peon(Color.BLANCO), Peon(Color.BLANCO), Peon(Color.BLANCO)], 
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            [Peon(Color.NEGRO), Peon(Color.NEGRO), Peon(Color.NEGRO), Peon(Color.NEGRO), Peon(Color.NEGRO), Peon(Color.NEGRO), Peon(Color.NEGRO), Peon(Color.NEGRO)], 
+            [Torre(Color.NEGRO), Caballo(Color.NEGRO), Alfil(Color.NEGRO), Reina(Color.NEGRO), Rey(Color.NEGRO), Alfil(Color.NEGRO), Caballo(Color.NEGRO), Torre(Color.NEGRO)], 
+        ]
+    def mover_pieza(self, casilla_inicial, casilla_final):
         fila_inicial, columna_inicial=casilla_inicial
         pieza=self.tablero[fila_inicial][columna_inicial]
         self.tablero[fila_inicial][columna_inicial]=" "
@@ -25,7 +25,7 @@ a=TableroAjedrez()
 for fila in a.tablero:
     print(fila)
 
-a.mover_pieza([0,4],[3,4])
+a.mover_pieza([0, 4], [3, 4])
 print("\n\n\n")
 for fila in a.tablero:
     print(fila)
